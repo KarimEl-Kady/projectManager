008e9f433d89b275ed8bccb178333cf34379a128
Karim <karim.elkady.kmk@gmail.com>
2026-08-03 23:57:13 +0300

    feat: queue overdue task notifications

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Modules\Task\Jobs\NotifyOverdueTasks;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new NotifyOverdueTasks)
+    ->dailyAt('08:00')
+    ->withoutOverlapping();
