4c78cf340da0a403467381ba76f8050019cbe1e4
Karim <karim.elkady.kmk@gmail.com>
2026-08-22 20:53:35 +0300

    update seeder

--- app/Modules/User/Database/Factories/UserFactory.php
+++ app/Modules/User/Database/Factories/UserFactory.php
@@ -6,6 +6,7 @@ use App\Modules\User\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as FakerFactory;
 
 /** @extends Factory<User> */
 class UserFactory extends Factory
@@ -19,10 +20,10 @@ class UserFactory extends Factory
     {
         return [
             'uuid' => (string) Str::uuid(),
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => FakerFactory::create()->name(),
+            'email' => FakerFactory::create()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'phone' => $this->faker->unique()->numerify('+2010#######'),
+            'phone' => FakerFactory::create()->unique()->numerify('+2010#######'),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
